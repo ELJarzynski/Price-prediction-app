9184a514161ef45fb4b665847f5dc5efa1417e43
YoungHedgehog <92035751+ELJarzynski@users.noreply.github.com>
2024-10-26 13:25:01 +0200

    Delete October7-UI-UX-Done directory

--- October7-UI-UX-Done/RealEstatePriceForecastBackend/RealEstatePriceForecastBackend/urls.py
+++
@@ -1,9 +0,0 @@
-# RealEstatePriceForecast/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('property/', include('property.urls')),  # Użyj nazw aplikacji bez pełnej ścieżki
-    path('user/', include('user.urls')),  # Przykładowe dodanie dla aplikacji 'user'
-]
