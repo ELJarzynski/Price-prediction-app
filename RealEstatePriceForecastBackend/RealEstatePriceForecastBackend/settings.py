9ab4a11286ed1a272416021ba96a557122ebdccc
YoungHedgehog <92035751+ELJarzynski@users.noreply.github.com>
2024-11-05 19:08:40 +0100

    privacy correction

--- RealEstatePriceForecastBackend/RealEstatePriceForecastBackend/settings.py
+++ RealEstatePriceForecastBackend/RealEstatePriceForecastBackend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Wczytaj zmienne środowiskowe z pliku .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_ez0$*z#r)n^n(!1d-6@*&w-%@=*z8w77+^hxq18&)4-jjb^v'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,11 +92,11 @@ WSGI_APPLICATION = 'RealEstatePriceForecastBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'realestatepriceforecast_db',
-        'USER': 'postgres',
-        'PASSWORD': 'kamil',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
